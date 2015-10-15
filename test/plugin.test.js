7e73cea3002eff55b11802a1d404b2b2495df30d
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-10-14 22:28:07 -0400

    Update transformation based on feedback

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -2,8 +2,11 @@ var babel  = require("babel")
 var plugin = require("../lib/index")
 var assert = require("assert")
 
-var answer = babel.transform("Object.is(NaN, NaN)", { plugins: [ plugin ] });
+describe('babel-plugin-object-is', function() {
 
-assert(answer.code.indexOf("require('object-is')(NaN, NaN)") >= 0)
+  it ('replaces Object.is with the npm package object-is', function() {
+    var answer = babel.transform("Object.is(NaN, NaN)", { plugins: [ plugin ] });
+    assert(answer.code.indexOf("require(\"object-is\")(NaN, NaN)") >= 0)
+  })
 
-console.log("Tests pass!")
+})
