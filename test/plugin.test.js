b0c57887a592760f75390c3753108b004036e553
Terence <terence@awesapp.com>
2017-11-21 17:56:20 +0800

    upgrade to babel 6

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,12 +1,12 @@
-var babel  = require("babel")
+var babel  = require("babel-core")
 var plugin = require("../lib/index")
 var assert = require("assert")
 
 describe('babel-plugin-object-is', function() {
 
   it ('replaces Object.is with the npm package object-is', function() {
-    var answer = babel.transform("Object.is(NaN, NaN)", { plugins: [ plugin ] });
-    assert(answer.code.indexOf("require(\"object-is\")(NaN, NaN)") >= 0)
+    var answer = babel.transform("if(Object.is('3', 3) === true);\nconst a = 1;", { plugins: [ plugin ] });
+    assert(answer.code.indexOf("require('object-is')('3', 3)") >= 0)
   })
 
 })
